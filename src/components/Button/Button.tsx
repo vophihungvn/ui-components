080cb086e18d2ee75ed0681ebdaad09a979c328c
Hung Vo <vphungkhtn@gmail.com>
2021-07-06 11:29:15 +0700

    Merge config

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,7 +5,7 @@ const Button = (props: any) => {
     <button
       // onClick={submitFailure}
       type="button"
-      className="inline-flex mt-4 uppercase tracking-widest items-center px-4 w-36 py-3 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+      className="inline-flex mt-4 uppercase tracking-widest items-center px-4 py-3 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
     >
       <span className="mr-auto">Submit</span>
     </button>
