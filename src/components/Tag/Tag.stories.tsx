b409eab4e7c4f0d9141fe6e267834d9e8d24dad9
Hung Vo <vphungkhtn@gmail.com>
2021-07-08 11:12:35 +0700

    feat: Tag component

--- src/components/Tag/Tag.stories.tsx
+++ src/components/Tag/Tag.stories.tsx
@@ -6,15 +6,12 @@ import { Tag } from "./Tag";
 export default {
   title: "Tags",
   component: Tag,
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
 } as ComponentMeta<typeof Tag>;
 
 const Template: ComponentStory<typeof Tag> = (args) => <Tag {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
-  label: "Tag",
+  title: "New",
+  className: "text-blue-700 bg-blue-50",
 };
--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,7 +1,23 @@
+import classnames from "classnames";
+
 import "./Tag.css";
 
-const Tag = (props: any) => {
-  return <div>Tag</div>;
+interface TagProps {
+  className?: string;
+  title?: string;
+}
+
+const Tag = (props: TagProps) => {
+  return (
+    <span
+      className={classnames(
+        "inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium",
+        props?.className ?? ""
+      )}
+    >
+      {props.title ?? ""}
+    </span>
+  );
 };
 
 export { Tag };
